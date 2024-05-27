8e347d5ec1ce742ecf266e32a68547c0f6cc4774
Philippe Vlérick <pvlerick@gmail.com>
2024-05-27 11:29:06 +0200

    missing command

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,8 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    if let Some(command) = input.split_whitespace().collect::<Vec<_>>().first() {
+        eprintln!("{}: command not found", command);
+    }
 }
