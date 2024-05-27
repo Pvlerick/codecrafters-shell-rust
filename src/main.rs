8cf18aa1eaadf34a27c5f1c56f710f41984f3dde
Philippe Vlérick <pvlerick@gmail.com>
2024-05-27 11:47:59 +0200

    repl

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
 
-    if let Some(command) = input.split_whitespace().collect::<Vec<_>>().first() {
-        eprintln!("{}: command not found", command);
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        stdin.read_line(&mut input).unwrap();
+
+        if let Some(command) = input.split_whitespace().collect::<Vec<_>>().first() {
+            eprintln!("{}: command not found", command);
+        }
     }
 }
