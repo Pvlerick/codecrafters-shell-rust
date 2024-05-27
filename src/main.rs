68cff14a3157bc4efb9127713297dc02e84d0f9f
Philippe Vlérick <pvlerick@gmail.com>
2024-05-27 11:52:13 +0200

    exit 0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     let stdin = io::stdin();
@@ -12,8 +13,13 @@ fn main() {
         input.clear();
         stdin.read_line(&mut input).unwrap();
 
-        if let Some(command) = input.split_whitespace().collect::<Vec<_>>().first() {
-            eprintln!("{}: command not found", command);
+        match input.as_str() {
+            "exit 0\n" => exit(0),
+            _ => {
+                if let Some(command) = input.split_whitespace().collect::<Vec<_>>().first() {
+                    eprintln!("{}: command not found", command);
+                }
+            }
         }
     }
 }
